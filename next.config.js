187864f6fdde97fb4b63c2e7bba414e54867d588
Bohdan <bogdank208@gmail.com>
2023-07-31 23:34:48 +0300

    asdasdasd

--- next.config.js
+++ next.config.js
@@ -2,7 +2,4 @@ module.exports = {
   // time in seconds of no pages generating during static
   // generation before timing out
   staticPageGenerationTimeout: 1000,
-  images: {
-    domains: ['www.wordstream.com'],
-}
 }
