10a49c3e9c2c553e13815b056464a85d97a5b1f3
Bohdan <bogdank208@gmail.com>
2023-08-01 21:46:51 +0300

    asd

--- next.config.js
+++ next.config.js
@@ -2,4 +2,14 @@ module.exports = {
   // time in seconds of no pages generating during static
   // generation before timing out
   staticPageGenerationTimeout: 1000,
+  images: {
+    remotePatterns: [
+      {
+          protocol: 'https',
+          hostname: '**',
+          port: '',
+          pathname: '**',
+      },
+    ],
+}
 }
